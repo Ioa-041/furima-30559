61c601ad2623afb9c327ccb0dbd9d7849b9e4c75
Ioa-041 <scraggly15@gmail.com>
2020-10-19 00:37:36 +0900

    Rubocopの実行、自動修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    last_name             {ForgeryJa::Name.last_name}
-    first_name            {ForgeryJa::Name.first_name}
-    last_name_kana        {ForgeryJa::Name.last_name(:to => ForgeryJa::KANA)}
-    first_name_kana       {ForgeryJa::Name.first_name(:to => ForgeryJa::KANA)}
-    birthday              {"1930-01-01"}
+    nickname              { Faker::Name.initials }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    last_name             { ForgeryJa::Name.last_name }
+    first_name            { ForgeryJa::Name.first_name }
+    last_name_kana        { ForgeryJa::Name.last_name(to: ForgeryJa::KANA) }
+    first_name_kana       { ForgeryJa::Name.first_name(to: ForgeryJa::KANA) }
+    birthday              { '1930-01-01' }
   end
 end
