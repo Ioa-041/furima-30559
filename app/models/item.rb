f82f326e5376ba26a339dd3921dcf97a6aebd35b
Ioa-041 <scraggly15@gmail.com>
2020-10-19 16:57:03 +0900

    ActiveStorageの導入とItemモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :prefecture, :sales_status, :svheduled, :shipping_fee_status
+  belongs_to :user
+  has_one_attached :image
 end
