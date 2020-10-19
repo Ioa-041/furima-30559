30a4734199c134bc27cc11e725262a3771f15d94
Ioa-041 <scraggly15@gmail.com>
2020-10-19 10:52:42 +0900

    FactoryBot修正とテストコード追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'abc123' }
     password_confirmation { password }
     last_name             { ForgeryJa::Name.last_name }
     first_name            { ForgeryJa::Name.first_name }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,6 +49,13 @@ describe User do
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
 
+      it 'emailに@がないと登録できない' do
+        @user.email = 'kkk.com'
+        @user.valid?
+        binding.pry
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
