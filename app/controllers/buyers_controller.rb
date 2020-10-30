bd4b3cf94a998accc3bff53ae38b5c1e85697bc9
Ioa-041 <scraggly15@gmail.com>
2020-10-30 16:05:24 +0900

    フォームの情報を保存する実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,4 +2,20 @@ class BuyersController < ApplicationController
   def index
     @buyer_shopping_address = BuyerShoppingAddress.new
   end
+
+  def create
+    @buyer_shopping_address = BuyerShoppingAddress.new(shopping_address_params)
+      if @buyer_shopping_address.valid?
+        @buyer_shopping_address.save
+        redirect_to controller: :items, action: :index
+      else
+        render action: :index
+      end
+  end
+
+  private
+
+  def shopping_address_params
+    params.require(:buyer_shopping_address).permit(:postal_code, :prefecture, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
