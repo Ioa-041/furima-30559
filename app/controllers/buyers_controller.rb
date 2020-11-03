0b70105262cbf783aff1dc4a4ed023eefe553439
Ioa-041 <scraggly15@gmail.com>
2020-11-03 16:32:02 +0900

    クレジットカード決済機能の実装

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,4 +1,6 @@
 class BuyersController < ApplicationController
+  before_action :set_item
+
   def index
     @buyer_shopping_address = BuyerShoppingAddress.new
     @item = Item.find(params[:item_id])
@@ -6,17 +8,31 @@ class BuyersController < ApplicationController
 
   def create
     @buyer_shopping_address = BuyerShoppingAddress.new(shopping_address_params)
-      if @buyer_shopping_address.valid?
-        @buyer_shopping_address.save
-        redirect_to controller: :items, action: :index
-      else
-        render action: :index
-      end
+    if @buyer_shopping_address.valid?
+      pay_item
+      @buyer_shopping_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
 
   def shopping_address_params
-    params.require(:buyer_shopping_address).permit(:postal_code, :prefecture, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buyer_shopping_address).permit(:postal_code, :prefecture, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: shopping_address_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/buyer_shopping_address.rb
+++ app/models/buyer_shopping_address.rb
@@ -1,6 +1,6 @@
 class BuyerShoppingAddress 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
@@ -8,6 +8,7 @@ class BuyerShoppingAddress
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A\d{11}\z/, message: 'Input only number' }
+    validates :token
   end
   validates :prefecture, numericality: { other_than: 1 }
 
